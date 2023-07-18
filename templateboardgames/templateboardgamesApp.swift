608f45eff36bcefc224cabd9ccc6bd9d1f7d2fc6
ElTinTin <tintin16200@gmail.com>
2023-07-18 14:15:46 +0200

    Initial Commit

---
+++ templateboardgames/templateboardgamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  templateboardgamesApp.swift
+//  templateboardgames
+//
+//  Created by Quentin Deschamps on 18/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct templateboardgamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
